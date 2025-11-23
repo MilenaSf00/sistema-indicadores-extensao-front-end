874d351c246d098fb5dea688a5b7851159e73ad1
dddias-kaua <kauadias.aluno@unipampa.edu.br>
2025-11-23 11:10:27 -0300

    Detalhes Visuais Tela Home
    Foram adicionados os detalhes visuais na tela home do site, inspirados na identidade visual da PROEC.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,12 @@ import '../css/Footer.css';
 const Footer: React.FC = () => {
   return (
     <footer className="dashboard-footer">
-      <div className="footer-content">
-        EQUIPE: Milena Soares Ferreira - Estudante do curso de Eng. Software | milenasf.aluno@unipampa.edu.br  
-        Kauã Jardim Dias - Estudante do curso Ciência da Computação | kauadias.aluno@unipampa.edu.br  
-        Aline Vieira de Mello - Docente | alinemello@unipampa.edu.br  
-        © 2025 | Indicadores da Extensão
+      <div>
+        <h4 className="footer-title">EQUIPE:</h4>
+        <p className="footer-content">Milena Soares Ferreira - Estudante do curso de Eng. Software | milenasf.aluno@unipampa.edu.br</p>
+        <p className="footer-content">Kauã Jardim Dias - Estudante do curso Ciência da Computação | kauadias.aluno@unipampa.edu.br </p>
+        <p className="footer-content">Aline Vieira de Mello - Docente | alinemello@unipampa.edu.br </p>
+        <p className='copyright'>© 2025 | Indicadores da Extensão </p>
       </div>
     </footer>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import '../css/Home.css'; 
 import logo from '../assets/LogoHome.png'; 
 import Footer from '../components/Footer';
+import LeftInfoGraphs from './LeftInfoGraphs';
+import RightInfoGraphs from './RightInfoGraphs';
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-
-      
       <img src={logo} alt="Logo" className="home-logo" style={{ zIndex: 2, position: "relative" }} />
 
       <div className="home-text" style={{ zIndex: 2, position: "relative" }}>
@@ -16,17 +16,23 @@ const Home: React.FC = () => {
         <h2 className="home-subsubtitle">
           Acompanhe os dados sobre as ações de extensão realizadas na UNIPAMPA
         </h2>
-
         <button className="home-button">Acessar Indicadores</button>
       </div>
-
       <div className="info-box" style={{ zIndex: 2, position: "relative" }}>
+        <div className='left-info-decorators' style={{ zIndex: 3, position: "relative"}}>
+        <LeftInfoGraphs/>
+      </div>
+      <div className='right-info-decorators' style={{ zIndex: 3, position: "relative"}}>
+        <RightInfoGraphs/>
+      </div>
         <h3 className="info-title">EXTENSÃO E CULTURA NA UNIPAMPA</h3>
         <p className="info-text">
           A Extensão Universitária, sob o princípio constitucional da indissociabilidade entre ensino, pesquisa e extensão, é um processo interdisciplinar, educativo, cultural, científico e político que promove a interação transformadora entre Universidade e outros setores da sociedade.
         </p>
+        <p className="info-reference">
+          Plano Nacional de Extensão Universitária, 1999.
+        </p>
       </div>
-
       <Footer />
     </div>
   );
