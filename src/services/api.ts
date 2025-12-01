e510169fe0981c617dba9c9eddc4749895e27016
MilenaSf00 <milenasoaresf2@gmail.com>
2025-12-01 04:25:15 -0300

    api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://sistema-indicadores-extensao-back-n852iieaa.vercel.app';
 
 
 export const signup = async (username: string, password: string) => {
