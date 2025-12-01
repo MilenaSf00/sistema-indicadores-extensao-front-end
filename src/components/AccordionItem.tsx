1d7c953892225994781e338fc948bcf5987468cf
dddias-kaua <kauadias.aluno@unipampa.edu.br>
2025-12-01 11:37:42 -0300

    pagina-de-ajuda

---
+++ src/components/AccordionItem.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+import type { AccordionData } from '../types';
+import '../css/AccordionItem.css';
+
+function AccordionItem({
+  data,
+  isOpen,
+  btnOnClick,
+}: {
+  data: AccordionData;
+  isOpen: boolean;
+  btnOnClick: () => void;
+}) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (isOpen) {
+      const contentEl = contentRef.current as HTMLDivElement;
+
+      setHeight(contentEl.scrollHeight);
+    } else {
+      setHeight(0);
+    }
+  }, [isOpen]);
+
+  return (
+    <li className={`accordion-item ${isOpen ? 'active' : ''}`}>
+      <h2 className="accordion-item-title">
+        <button className="accordion-item-btn" onClick={btnOnClick}>
+          <strong>{data.title}</strong>
+        </button>
+      </h2>
+      <div className="accordion-item-container" style={{ height }}>
+        <div ref={contentRef} className="accordion-item-content">
+          {data.content}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default AccordionItem;
